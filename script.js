61fec65ff38599f65215b7e5bd6233549aa615f6
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2022-11-26 15:59:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const hourTime = prompt('Введіть кількість годин');
+
+const secTime = 3600;
+
+const resultTime = hourTime * secTime;
+
+const finalValuet = resultTime;
+
+alert(finalValuet + ' cекунд');
+
